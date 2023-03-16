e23d98009ad2f7eca97e333e71b3d29811787082
KSI5 <krissigebs@gmail.com>
2023-03-16 09:05:01 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - rogramming is like building a multilingual
+ *                puzzle, followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 { 
-puts("\"Programming is like building a multilingual puzz
-le");
-return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+
 }
