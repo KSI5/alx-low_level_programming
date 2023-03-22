bd344587aefef1618d81d072a892f3b35e218256
KSI5 <krissigebs@gmail.com>
2023-03-22 04:30:08 -0700

    updated 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,6 +5,7 @@
 */
 int main(void)
 {
+
 int i;
 
 printf("Infinite loop incoming :(\n");
@@ -13,6 +14,7 @@ i = 0;
 /*{*/
 /*putchar(i);*/
 /*}*/
+
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
