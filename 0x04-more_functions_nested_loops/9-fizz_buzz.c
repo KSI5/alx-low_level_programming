a7c0495ea82c4897a27dd7fbf3050b7825eee2d8
KSI5 <krissigebs@gmail.com>
2023-03-23 09:39:44 -0700

    fixed 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,41 +1,27 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
-*main- Prints numbers from 1 to 100 and replaces multiples of
+*main - Entry point
 *
-*3, 5, and 15 with Fizz, Buzz, and FizzBuzz, respectively.
-*
-*Returns: Always 0 (Success)
+*Return: Always 0 (Success)
 */
-
 int main(void)
-
 {
-
 int i;
 
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-{
-
 printf("FizzBuzz ");
-}
 else if (i % 3 == 0)
-{
 printf("Fizz ");
-}
 else if (i % 5 == 0)
-{
 printf("Buzz ");
-}
 else
-{
 printf("%d ", i);
 }
-}
 printf("\n");
+
 return (0);
 }
 
