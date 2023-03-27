527e4128a3387e2a316aa9495eaf026eb3c5d1a8
KSI5 <krissigebs@gmail.com>
2023-03-27 05:17:29 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value to which the parameter points to to 98
+ *
+ * @n: a pointer to the integer to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Pointer to first integer.
+ * @b: Pointer to second integer.
+ *
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
+
