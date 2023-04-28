ae8425f1cca30ecf3057e0ae9a4aefdbfccba06e
KSI5 <krissigebs@gmail.com>
2023-04-28 05:26:37 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+    hello db "Hello, Holberton", 0
+section .text
+    global main
+    extern printf
+main:
+    push rbp
+    mov rbp, rsp
+    mov edi, hello
+    xor eax, eax
+    call printf
+    xor eax, eax
+    pop rbp
+    ret
+
