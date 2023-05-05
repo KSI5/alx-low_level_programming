2ae0aa68167de0c482f91da277bce2c05ee41e33
KSI5 <krissigebs@gmail.com>
2023-05-05 11:44:17 -0700

    updated the main.header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -12,7 +12,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int _atoi(const char *s);
 /* Function to determine the endianness of the machine */
 int get_endianness(void);
 
