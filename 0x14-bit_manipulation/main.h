673b4e49c9dfbe801af734274f26bef711f6a704
KSI5 <krissigebs@gmail.com>
2023-05-05 06:07:14 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/* function prototypes */
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -8,6 +12,7 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+/* Function to determine the endianness of the machine */
 int get_endianness(void);
-#endif /* MAIN_H */ 
 
+#endif /* _MAIN_H_ */
