81c01751202826dcf225f9efd27e445b69132e96
KSI5 <krissigebs@gmail.com>
2023-06-19 15:17:44 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-(sleep 98 && echo '9 8 10 24 75 9' > numbers.txt && echo 'Please make me win!') &
-
-wget -q -O gm https://github.com/alx-tools/0x18.c/raw/master/gm && chmod +x gm
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
